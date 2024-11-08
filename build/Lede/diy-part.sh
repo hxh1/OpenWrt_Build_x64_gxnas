6930f88c3d14434f9d4e80f8c69a9f1a204b991a
歌者 <34333540+hxh1@users.noreply.github.com>
2024-11-08 18:01:20 +0800

    Update diy-part.sh

--- build/Lede/diy-part.sh
+++ build/Lede/diy-part.sh
@@ -35,7 +35,7 @@ sed -i 's/192.168.1.1/192.168.8.10/g' package/base-files/luci2/bin/config_genera
 
 
 # 后台IP设置
-export Ipv4_ipaddr="192.168.8.8"            # 修改openwrt后台地址(填0为关闭)
+#export Ipv4_ipaddr="192.168.8.8"            # 修改openwrt后台地址(填0为关闭)
 export Netmask_netm="255.255.255.0"          # IPv4 子网掩码（默认：255.255.255.0）(填0为不作修改)
 export Op_name="OpenWrt"               # 修改主机名称为OpenWrt-123(填0为不作修改)
 
